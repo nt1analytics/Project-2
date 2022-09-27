1e04a24ff1ec9c9a64a07119f05b71da5e29afdf
Aidan Thompson <Aidan.Thompson1@gmail.com>
2022-09-28 01:29:18 +0800

    join test

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS listings_host
+DROP TABLE IF EXISTS calendar
 
 CREATE TABLE listings_host (
 id INT PRIMARY KEY,
@@ -7,3 +8,11 @@ host_acceptance_rate VARCHAR,
 host_is_superhost VARCHAR,
 zipcode INT
 );
+
+
+--join test--
+SELECT calendar.listing_id, calendar.date, calendar.available, 
+calendar.price, listings_host.host_response_rate, listings_host.host_acceptance_rate
+FROM calendar
+JOIN listings_host
+ON calendar.listing_id = listings_host.id
