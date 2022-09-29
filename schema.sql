591961b50959a6aa8d136ab43b4df396bdc0ab9b
Aidan Thompson <Aidan.Thompson1@gmail.com>
2022-09-29 20:01:37 +0800

    changes

--- schema.sql
+++ schema.sql
@@ -17,7 +17,7 @@ DROP TABLE IF EXISTS calendar
 -- Creating the Table, setting primary key and types on columns. 
 
 CREATE TABLE calendar( 
-listing_id int,
+listing_id INT,
 date DATE,
 available TEXT,
 price DECIMAL 
@@ -35,10 +35,25 @@ DROP TABLE IF EXISTS listings_reviews
 -- Creating the Table, setting primary key and types on columns.
 
 CREATE TABLE listings_reviews( 
-id int PRIMARY KEY,
+id INT PRIMARY KEY,
 number_of_reviews INT,
 review_scores_rating DECIMAL 
 );
+
+DROP TABLE IF EXISTS calendar
+
+-- Creating the Table, setting primary key and types on columns. 
+
+CREATE TABLE type_and_price( 
+id INT PRIMARY KEY,
+property_type TEXT,
+room_type TEXT,
+accommodates INT
+square_feet INT
+weekly_price INT
+monthly_price INT
+);
+
 
 -- Check that the Table has been properly created.
 
